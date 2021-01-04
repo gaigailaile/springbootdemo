260418828116470344753909fe79ef09d6f8fb98
gaidongxu <gaidongxu@dataexa.com>
2021-01-05 00:02:18 +0800

    3中启动方式练习

---
+++ src/main/java/com/gai/DemoApplication2.java
@@ -0,0 +1,13 @@
+//package com.gai;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//@EnableAutoConfiguration
+//@ComponentScan("com.gai")
+//public class DemoApplication2 {
+//    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication2.class,args);
+//    }
+//}
