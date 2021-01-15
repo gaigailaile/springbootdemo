9713702158d95038ec30fa6cc7041dfccb4e1578
gaidongxu <gaidongxu@dataexa.com>
2021-01-15 18:41:19 +0800

    2021.1.15 nginx练习

--- src/main/java/com/gai/controller/HelloController.java
+++ src/main/java/com/gai/controller/HelloController.java
@@ -1,16 +1,22 @@
 package com.gai.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 //@EnableAutoConfiguration
 public class HelloController {
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping("/hello")
-    public String helle(){
-        return "hello world";
+    public String hello(HttpServletRequest request){
+        return port + " say hello world";
     }
 
 //    public static void main(String[] args) {
